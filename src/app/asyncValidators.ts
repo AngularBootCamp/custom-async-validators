72102b09e6e3f307e67f43e175afae00e0395582
Lance Finney <lance.finney@oasisdigital.com>
2018-03-02 22:55:07 -0600

    curriculum update

--- src/app/asyncValidators.ts
+++ src/app/asyncValidators.ts
@@ -1,27 +1,23 @@
 import { HttpClient } from '@angular/common/http';
-import { FormControl } from '@angular/forms';
+import { FormControl, ValidationErrors } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { catchError, delay, map, tap } from 'rxjs/operators';
 
-interface IValidationResult {
-  [key: string]: any;
-}
-
-export function simpleAsyncValidator(): Observable<IValidationResult> {
+export function simpleAsyncValidator(): Observable<ValidationErrors> {
   // return of({ simpleAsyncValidator: 'blew up' });
   // OK
   return of(undefined);
 }
 
-export function slowAsyncValidator(): Observable<IValidationResult> {
+export function slowAsyncValidator(): Observable<ValidationErrors> {
   return of(undefined).pipe(delay(1000));
 }
 
 const url = 'https://api.zippopotam.us/us/';
 
 export function westernZipValidatorFactory(http: HttpClient) {
-  return (control: FormControl): Observable<IValidationResult> => {
+  return (control: FormControl): Observable<ValidationErrors> => {
     return http.get<any>(url + control.value).pipe(
       tap(r => console.log(r)),
       map(data => data.places[0].longitude),
