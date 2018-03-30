bb9fb19546c9a05c8536589a64431a6f467c3160
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-03-30 14:27:19 -0500

    curriculum update

--- src/app/asyncValidators.ts
+++ src/app/asyncValidators.ts
@@ -4,27 +4,32 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { catchError, delay, map, tap } from 'rxjs/operators';
 
-export function simpleAsyncValidator(): Observable<ValidationErrors> {
+// Many TypeScript developers recommend always using undefined rather
+// than nul; however, the Angular documentation specifically states
+// that and a sink validator should return an observable of either
+// ValidationErrors or null.
+
+export function simpleAsyncValidator(): Observable<ValidationErrors | null> {
   // return of({ simpleAsyncValidator: 'blew up' });
   // OK
-  return of(undefined);
+  return of(null);
 }
 
-export function slowAsyncValidator(): Observable<ValidationErrors> {
-  return of(undefined).pipe(delay(1000));
+export function slowAsyncValidator(): Observable<ValidationErrors | null> {
+  return of(null).pipe(delay(1000));
 }
 
 const url = 'https://api.zippopotam.us/us/';
 
 export function westernZipValidatorFactory(http: HttpClient) {
-  return (control: FormControl): Observable<ValidationErrors> => {
+  return (control: FormControl): Observable<ValidationErrors | null> => {
     return http.get<any>(url + control.value).pipe(
       tap(r => console.log(r)),
       map(data => data.places[0].longitude),
       map(l => l < -90),
       tap(ok => ok ? console.log('It is west enough') : console.log('It is not west enough')),
-      map(ok => ok ? undefined : { westerliness: 'not enough' }),
-      catchError(e => {
+      map(ok => ok ? null : { westerliness: 'not enough' }),
+      catchError(_e => {
         return of({ westerliness: 'Unable to verify' });
       }));
   };
